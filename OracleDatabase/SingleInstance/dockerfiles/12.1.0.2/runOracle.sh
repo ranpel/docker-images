e8462b0f5b954965d031fb05affa6f32b0c5e602
Randy Pelletier <randy.pelletier@oracle.com>
2018-10-16 17:37:40 -0400

    Missed underscore placement in trace file name take two. Fixed again.

--- OracleDatabase/SingleInstance/dockerfiles/12.1.0.2/runOracle.sh
+++ OracleDatabase/SingleInstance/dockerfiles/12.1.0.2/runOracle.sh
@@ -232,7 +232,7 @@ if [ -f "$ORACLE_BASE/.db_configured" ]; then
   echo
   echo "The following output is now a tail of the alert.log:"
   # This is just not doing it for me.. how about some beef with a bun and a pickle?
-  if [ -f $ORACLE_BASE/diag/rdbms/${ORACLE_SID}/${ORACLE_SID}/trace/alert${ORACLE_SID}.log ]; then
+  if [ -f $ORACLE_BASE/diag/rdbms/${ORACLE_SID}/${ORACLE_SID}/trace/alert_${ORACLE_SID}.log ]; then
     tail -f $ORACLE_BASE/diag/rdbms/${ORACLE_SID}/${ORACLE_SID}/trace/alert_${ORACLE_SID}.log &
     childPID=$!
     wait $childPID
