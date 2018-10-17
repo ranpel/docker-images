036c4956128b0c01c99fbda09f468c629898337d
Randy Pelletier <randy.pelletier@oracle.com>
2018-10-17 16:12:26 -0400

    Minor groom - stripped some unused/unneeded items from runOracle.sh

--- OracleDatabase/SingleInstance/dockerfiles/12.1.0.2/runOracle.sh
+++ OracleDatabase/SingleInstance/dockerfiles/12.1.0.2/runOracle.sh
@@ -11,68 +11,6 @@
 # 
 # October 2018: Dyn DNS Dev (ddd) - adjust to custom CREATE DATABASE requirement for option trimmed dns db
 
-########### Move DB files ############
-function copyFiles {
-
-  if [ ! -d $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID ]; then
-     mkdir -p $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/
-  fi;
-
-   
-  if [ -f $ORACLE_HOME/dbs/spfile$ORACLE_SID.ora ]; then
-    cp $ORACLE_HOME/dbs/spfile$ORACLE_SID.ora $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/
-  elif [ -f $ORACLE_HOME/dbs/init${ORACLE_SID}.ora ]; then
-    cp $ORACLE_HOME/dbs/init${ORACLE_SID}.ora $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/
-  fi
-  if [ -f $ORACLE_HOME/dbs/orapw$ORACLE_SID ]; then
-    cp $ORACLE_HOME/dbs/orapw$ORACLE_SID $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/
-  fi
-  if [ -f $ORACLE_HOME/network/admin/sqlnet.ora ]; then
-    cp $ORACLE_HOME/network/admin/sqlnet.ora $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/
-  fi
-  if [ -f $ORACLE_HOME/network/admin/listener.ora ]; then
-    cp $ORACLE_HOME/network/admin/listener.ora $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/
-  fi
-  if [ -f $ORACLE_HOME/network/admin/tnsnames.ora ]; then
-    cp $ORACLE_HOME/network/admin/tnsnames.ora $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/
-  fi
-
-  # oracle user does not have permissions in /etc, hence cp and not mv
-  # we need to touch this higher up - hrm..
-  cp /etc/oratab $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/
-   
-  # (ddd delete - can't quite see the value, yet)
-  #symLinkFiles;
-}
-
-########### Symbolic link DB files ############
-function symLinkFiles {
-
-   if [ ! -L $ORACLE_HOME/dbs/spfile$ORACLE_SID.ora ]; then
-      ln -s $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/spfile$ORACLE_SID.ora $ORACLE_HOME/dbs/spfile$ORACLE_SID.ora
-   fi;
-   
-   if [ ! -L $ORACLE_HOME/dbs/orapw$ORACLE_SID ]; then
-      ln -s $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/orapw$ORACLE_SID $ORACLE_HOME/dbs/orapw$ORACLE_SID
-   fi;
-   
-   if [ ! -L $ORACLE_HOME/network/admin/sqlnet.ora ]; then
-      ln -s $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/sqlnet.ora $ORACLE_HOME/network/admin/sqlnet.ora
-   fi;
-
-   if [ ! -L $ORACLE_HOME/network/admin/listener.ora ]; then
-      ln -s $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/listener.ora $ORACLE_HOME/network/admin/listener.ora
-   fi;
-
-   if [ ! -L $ORACLE_HOME/network/admin/tnsnames.ora ]; then
-      ln -s $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/tnsnames.ora $ORACLE_HOME/network/admin/tnsnames.ora
-   fi;
-
-   # oracle user does not have permissions in /etc, hence cp and not ln 
-   cp $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/oratab /etc/oratab
-
-}
-
 ########### SIGINT handler ############
 function _int() {
    echo "Stopping container."
@@ -170,14 +108,6 @@ export ORACLE_CHARACTERSET=${ORACLE_CHARACTERSET:-AL32UTF8}
 # Check whether database already exists
 if [ -f $ORACLE_BASE/.db_configured ]; then
   if [ -d $ORACLE_BASE/oradata/$ORACLE_SID ]; then
-    # (ddd delete)
-    #symLinkFiles;
-   
-    # Make sure audit file destination exists
-    #if [ ! -d $ORACLE_BASE/admin/$ORACLE_SID/adump ]; then
-    #  mkdir -p $ORACLE_BASE/admin/$ORACLE_SID/adump
-    #fi;
-  
     # Start database
     $ORACLE_BASE/$START_FILE;
   else
@@ -193,12 +123,6 @@ else
   # Create database
   # (ddd - this is now CREATE DATABASE custom crdb)
   $ORACLE_BASE/$CREATE_DB_FILE $ORACLE_SID $ORACLE_PDB $ORACLE_PWD;
-   
-  # (ddd delete)
-  # Move database operational files to oradata
-  # not sure I need or want - no dbca - unsure of original intent.. doesn't seem legit.
-  #moveFiles;
-  copyFiles;
    
   # Execute custom provided setup scripts
   # we don't have these yet but .. good to know, thx
